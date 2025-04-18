839e7102a9844d4018d398247a7bb900c96ad754
Usman Mohsin <iamusmanmohsin@gmail.com>
2025-04-18 14:46:03 +0500

    End of Section 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,34 +1,31 @@
-
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports :[],
+  imports: [NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class  AppComponent implements OnInit {
-
-  http = inject(HttpClient);
-  title = 'DatingApp';
-  users: any;
+  private accountService = inject(AccountService);
+  
 
   ngOnInit(): void {
-    // You can use HTTP or HTTPS based on your backend setup
-    this.http.get('http://localhost:5000/api/users').subscribe({
-      next: response => {
-        this.users = response;
-        console.log("Users fetched:", this.users);
-      },
-      error: error => {
-        console.error("Backend request failed:", error);
-        alert("Something went wrong: " + JSON.stringify(error));
-      },
-      complete: () => console.log('Request has Completed')
-    });
+     
+     this.setCurrentUser();
   }
+
+   setCurrentUser() {
+        const userString =  localStorage.getItem('user');
+        if(!userString) return;
+        const user = JSON.parse(userString);
+        this.accountService.currentUser.set(user);
+   }
+  
 }
 
