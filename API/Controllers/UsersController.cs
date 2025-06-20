c37b7057ee55e2cbf289bc65a1228f4272d10408
Usman Mohsin <iamusmanmohsin@gmail.com>
2025-06-20 16:36:31 +0500

    End Of Section  10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,23 +11,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
-    {
+[Authorize]
+public class UsersController(IUserRepository userRepository , IMapper mapper) : BaseApiController
+{
 
-       
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() 
-        {
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
         var users = await userRepository.GetMembersAsync();
 
 
-            return Ok(users);
-        }
+        return Ok(users);
+    }
 
-        [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
         var user = await userRepository.GetMemberAsync(username);
 
         if (user is null) return NotFound();
@@ -34,6 +35,27 @@ namespace API.Controllers;
         return user;
 
     }
+
+    [HttpPut]
+
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
+     }
+
     }
 
 
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient} from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,35 @@ export class MembersService {
 
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
-  
-   getMembers(){
-       return this.http.get<Member[]>(this.baseUrl + 'users');
-   }
+  members = signal<Member[]>([]);
 
-    getMember(username:string){
-       return this.http.get<Member>(this.baseUrl + 'users/' + username);
-   }
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: members => this.members.set(members)
+    })
+  }
+
+  getMember(username: string) {
+    const member = this.members().find(x => x.userName === username);
+    if (member !== undefined) return of(member);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+
+  updateMember(member: Member) {
+
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+
+      tap(() => {
+
+        this.members.update(members => members.map(m => m.userName === member.userName
+
+          ? member : m))
+
+      })
+
+    )
+
+  }
 
 }
